4f06895c587670884659f3a26661f453b71761cb
Darius Kramer <dev@dkramer.fastmail.com>
2016-03-02 15:15:54 +0100

    Add colors for output

--- vittf.h
+++ vittf.h
@@ -8,7 +8,7 @@
 #define v_assert(expression) \
 	do { \
 		if (!(expression)) {\
-			printf("Failed:\tFILE(%s), LINE(%d), Expression(%s)\n", __FILE__, __LINE__, #expression); \
+			printf("\x1b[41mFailed >>>\x1b[0m FILE(%s), LINE(%d), Expression(%s)\n", __FILE__, __LINE__, #expression); \
 			exit(1); \
 		} \
 	} while (0)
@@ -16,14 +16,14 @@
 #define v_assert_str(expected, actual) \
 	do { \
 		if (strcmp((expected), (actual)) != 0) {\
-			printf("Failed:\tFILE(%s), LINE(%d), Expression(%s != %s)", __FILE__, __LINE__, (expected), (actual)); \
+			printf("\x1b[41mFailed>\x1b[0m\tFILE(%s), LINE(%d), Expression(%s != %s)", __FILE__, __LINE__, (expected), (actual)); \
 			exit(1);\
 		}\
 	} while (0)
 
 #define v_test_success(name) (printf("%s ✓\n", (name)))
 #define v_suite_success(suite) (printf("---> Suite %s ✓\n\n", (suite)))
-#define v_full_success(test) (printf("=========\nFULL TEST FOR %s ✓\n", (test)))
+#define v_full_success(test) (printf("=========\n\x1b[32mFULL TEST FOR %s ✓\x1b[0m\n", (test)))
 
 /*
  * Stdout Redirection
